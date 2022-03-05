079de4805a020a962c6435bf3c073562562c78ff
Softcorp Automation <dev@softcorp.io>
2022-03-05 17:02:00 +0000

    updated protobuf files

--- go_nuntio/nuntio_connect/nuntio_connect.pb.go
+++ go_nuntio/nuntio_connect/nuntio_connect.pb.go
@@ -7,7 +7,7 @@
 package nuntio_connect
 
 import (
-	nuntio "./go_nuntio/nuntio"
+	"github.com/softcorp-io/nuntio-proto/go_nuntio/nuntio"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
